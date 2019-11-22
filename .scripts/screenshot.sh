de2c3827807eedd4d6e64fed830b9ee8cc676bca
deadplan <50933540+deadplan@users.noreply.github.com>
2019-11-22 01:47:02 +0100

    added clipboard functionality

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -16,6 +16,7 @@ scrot -q 100 -s -f ~/Pictures/Screenshots/$folder/$name.png
 if [ -f ~/Pictures/Screenshots/$folder/$name.png ]
 then 
 	notify-send "Screenshot done! Name: $name"
+	xclip -selection clipboard -t image/png ~/Pictures/Screenshots/$folder/$name.png
 else
 	notify-send "Screenshot failed..."
 fi
